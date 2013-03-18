b5994bb2d20b2397d505cdd4f8fe63da4371b555
Adam Bass <your.adam2277@live.missouristate.edu>
2013-03-17 23:04:54 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,14 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -11,15 +19,7 @@ describe "User pages" do
     it { should have_selector('title', text: 'Sign up') }
   end
 
-  describe "profile page" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
-
-  	it { should have_selector('h1', text: user.name) }
-  	it { should have_selector('title', text: user.name) }
-  end
-
-   describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -28,6 +28,13 @@ describe "User pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -41,6 +48,15 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
